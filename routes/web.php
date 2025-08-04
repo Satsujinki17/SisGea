df6b6dac13ec565a79879a0724bf4fd29e032d39
Satsujinki17 <nievesleonsamuel@gmail.com>
2025-08-04 14:47:08 -0400

    Estoy aprendiendo a trabajar con los controladores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use app\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('Welcome');
 });
+
+
+Route::get('/users', [UserController::class,'index']);
